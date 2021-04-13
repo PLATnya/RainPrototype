be696f87718284a0a8e849807c829c37dc186f31
V <platinumcool777@gmail.com>
2021-04-13 14:06:14 +0300

    collision system namespace

--- Assets/Scripts/CollisionSystem.cs
+++ Assets/Scripts/CollisionSystem.cs
@@ -1,53 +1,57 @@
 using Rain.Components;
-
 using Unity.Entities;
 using Unity.Jobs;
-using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Physics.Systems;
-using UnityEngine;
 
-[UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
-[UpdateBefore(typeof(StepPhysicsWorld))]
-public class CollisionSystem : SystemBase
+namespace Rain.Systems
 {
-    private EndSimulationEntityCommandBufferSystem ecbSystem;
-    StepPhysicsWorld m_StepPhysicsWorld;
-    protected override void OnCreate()
+    [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+    [UpdateBefore(typeof(StepPhysicsWorld))]
+    public class CollisionSystem : SystemBase
     {
-        m_StepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
-        ecbSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-    }
+        private EndSimulationEntityCommandBufferSystem ecbSystem;
+        StepPhysicsWorld m_StepPhysicsWorld;
 
-    struct CollisionJob:ICollisionEventsJob
-    {
-        public EntityCommandBuffer commandBuffer;
-        public ComponentDataFromEntity<PlayerComponent> playerData;
-        public void Execute(CollisionEvent collisionEvent)
+        protected override void OnCreate()
         {
-            
-            bool IsAPlayer = playerData.HasComponent(collisionEvent.EntityA);
-            bool IsBPlayer = playerData.HasComponent(collisionEvent.EntityB);
-            Entity playerEntity = Entity.Null;
-            if (IsAPlayer || IsBPlayer)
-            {
-                playerEntity = IsAPlayer ? collisionEvent.EntityA : collisionEvent.EntityB;
-                commandBuffer.DestroyEntity(playerEntity);
-            }
-            
+            m_StepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
+            ecbSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
         }
-    }
-    protected override void OnUpdate()
-    {
-        SimulationCallbacks.Callback testCollisionEventsCallback =
-            (ref ISimulation simulation, ref PhysicsWorld world, JobHandle inDeps) =>
+
+        struct CollisionJob : ICollisionEventsJob
+        {
+            public EntityCommandBuffer commandBuffer;
+            public ComponentDataFromEntity<PlayerComponent> playerData;
+
+            public void Execute(CollisionEvent collisionEvent)
             {
-                return new CollisionJob
+
+                bool IsAPlayer = playerData.HasComponent(collisionEvent.EntityA);
+                bool IsBPlayer = playerData.HasComponent(collisionEvent.EntityB);
+                Entity playerEntity = Entity.Null;
+                if (IsAPlayer || IsBPlayer)
                 {
-                    commandBuffer = ecbSystem.CreateCommandBuffer(),
-                    playerData = GetComponentDataFromEntity<PlayerComponent>()
-                }.Schedule(simulation, ref world, inDeps);
-            };
-        m_StepPhysicsWorld.EnqueueCallback(SimulationCallbacks.Phase.PostSolveJacobians, testCollisionEventsCallback, Dependency);
+                    playerEntity = IsAPlayer ? collisionEvent.EntityA : collisionEvent.EntityB;
+                    commandBuffer.DestroyEntity(playerEntity);
+                }
+
+            }
+        }
+
+        protected override void OnUpdate()
+        {
+            SimulationCallbacks.Callback testCollisionEventsCallback =
+                (ref ISimulation simulation, ref PhysicsWorld world, JobHandle inDeps) =>
+                {
+                    return new CollisionJob
+                    {
+                        commandBuffer = ecbSystem.CreateCommandBuffer(),
+                        playerData = GetComponentDataFromEntity<PlayerComponent>()
+                    }.Schedule(simulation, ref world, inDeps);
+                };
+            m_StepPhysicsWorld.EnqueueCallback(SimulationCallbacks.Phase.PostSolveJacobians,
+                testCollisionEventsCallback, Dependency);
+        }
     }
 }
