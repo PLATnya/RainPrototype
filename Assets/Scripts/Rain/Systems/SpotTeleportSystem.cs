741055843e4e2268d6305f93a03651408a791535
V <platinumcool777@gmail.com>
2021-04-13 20:18:03 +0300

    spot teleport system - refactoring of code style, using Rain.Systems namespace

--- Assets/Scripts/Rain/Systems/SpotTeleportSystem.cs
+++ Assets/Scripts/Rain/Systems/SpotTeleportSystem.cs
@@ -1,62 +1,64 @@
 using Rain.Components;
 using Unity.Collections;
 using Unity.Entities;
-using Unity.Physics;
 using Unity.Transforms;
 
-public class SpotTeleportSystem : SystemBase
+
+namespace Rain.Systems
 {
-
-    private bool teleport;
-    protected override void OnUpdate()
+    public class SpotTeleportSystem : SystemBase
     {
-        NativeArray<float> checkPointsArray = new NativeArray<float>(GameManager.Instance.backPoints, Allocator.TempJob);
-        
-        /*NativeArray<float> backPoints =new NativeArray<float>(4, Allocator.TempJob) ;
-        backPoints[0] = 10;
-        backPoints[1] = 0;
-        backPoints[2] = -10;
-        backPoints[3] = GameManager.Instance.halfDelta;*/
 
-        NativeArray<bool> teleportArray = new NativeArray<bool>(1, Allocator.TempJob);
-        teleportArray[0] = teleport;
-        var ecbSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-        var cmb = ecbSystem.CreateCommandBuffer();
-        var playerData = GetComponentDataFromEntity<PlayerComponent>();
-        var teleportJob = Entities.WithAll<SpotComponent>().ForEach(
-            (int entityInQueryIndex,Entity entity, ref Translation translation, ref SpotComponent spotComponent) =>
-            {
-                if (playerData.HasComponent(entity))
+        private bool _teleport;
+
+        protected override void OnUpdate()
+        {
+            //game field limit and spawn points to native array, prepeared for job injection
+            NativeArray<float> checkingPointsArray =
+                new NativeArray<float>(GameManager.Instance.backPoints, Allocator.TempJob);
+
+            //the boolean meaning of the need for a teleport in the native array, prepeared for job injection
+            NativeArray<bool> teleportArray = new NativeArray<bool>(1, Allocator.TempJob) {[0] = _teleport};
+
+            var ecbSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+            var cmb = ecbSystem.CreateCommandBuffer();
+            var playerData = GetComponentDataFromEntity<PlayerComponent>();
+            var teleportJob = Entities.WithAll<SpotComponent>().ForEach(
+                (int entityInQueryIndex, Entity entity, ref Translation translation, ref SpotComponent spotComponent) =>
                 {
-                    if (!teleportArray[0])
+                    if (playerData.HasComponent(entity))
                     {
-                        if (translation.Value.y <= checkPointsArray[2])
+                        if (!teleportArray[0])
                         {
-                            teleportArray[0] = true;
+                            if (translation.Value.y <= checkingPointsArray[2])
+                            {
+                                teleportArray[0] = true;
+                            }
+                        }
+                        else
+                        {
+                            teleportArray[0] = false;
                         }
                     }
-                    else
+
+                    if (teleportArray[0])
                     {
-                        teleportArray[0] = false;
+                        float halfDelta = checkingPointsArray[3];
+                        if (translation.Value.y <=
+                            checkingPointsArray[1] - halfDelta)
+                            translation.Value.y += halfDelta * 4;
+                        else if (translation.Value.y > checkingPointsArray[0] - halfDelta &&
+                                 translation.Value.y < checkingPointsArray[0] + halfDelta)
+                        {
+                            cmb.DestroyEntity(entity);
+                        }
                     }
-                }
-                if (teleportArray[0])
-                {
-                    float halfDelta = checkPointsArray[3];
-                    if (translation.Value.y <=
-                        checkPointsArray[1] - halfDelta)
-                        translation.Value.y += halfDelta * 4;
-                    else if (translation.Value.y > checkPointsArray[0] - halfDelta &&
-                             translation.Value.y < checkPointsArray[0] + halfDelta)
-                    {
-                        cmb.DestroyEntity(entity);
-                    }
-                }
-            }).WithDisposeOnCompletion(checkPointsArray).WithBurst().Schedule(Dependency);
-        Dependency = teleportJob;
-        ecbSystem.AddJobHandleForProducer(Dependency);
-        teleportJob.Complete();
-        teleport = teleportArray[0];
-        teleportArray.Dispose();
+                }).WithDisposeOnCompletion(checkingPointsArray).WithBurst().Schedule(Dependency);
+            Dependency = teleportJob;
+            ecbSystem.AddJobHandleForProducer(Dependency);
+            teleportJob.Complete();
+            _teleport = teleportArray[0];
+            teleportArray.Dispose();
+        }
     }
 }
